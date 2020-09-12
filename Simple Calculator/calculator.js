00d6588399fa4425dc8b8d0cc90eaef980b34d26
PaulDSS <bishaldutta64@gmail.com>
2020-09-12 11:12:07 +0530

    Simple Calculator

--- Simple Calculator/calculator.js
+++ Simple Calculator/calculator.js
@@ -1,48 +1,57 @@
 var inputString1 = "";
 var inputString2 = "";
-var opert;
+var temp = "";
+var opert = "";
 var calc;
 
 function input(s1){
-        inputString1 += s1;
+    inputString1 += s1;
     document.getElementById("display").value = inputString1;
 }
 
 function operator(op){
-    inputString2 = inputString1;
-    document.getElementById("display").value = op;
-    opert = op;
-    inputString1 = "";
+    if(inputString1==="" && opert===""){
+        inputString2 = calc;
+        document.getElementById("display").value = op;
+        opert = op;
+    }else{
+        inputString2 = inputString1;
+        document.getElementById("display").value = op;
+        opert = op;
+        inputString1 = "";
+    }
 }
 
 function calculate(){
     if(opert==="+"){        
     calc = parseInt(inputString2)+parseInt(inputString1);
     document.getElementById("display").value = calc;
-    inputString1 = calc;
     }
 
     if(opert==="*"){        
         calc = parseInt(inputString2)*parseInt(inputString1);
         document.getElementById("display").value = calc;
-        inputString1 = calc;
     } 
 
     if(opert==="-"){        
         calc = parseInt(inputString2)-parseInt(inputString1);
         document.getElementById("display").value = calc;
-        inputString1 = calc;
     } 
 
     if(opert==="/"){        
         calc = parseInt(inputString2)/parseInt(inputString1);
         document.getElementById("display").value = calc;
-        inputString1 = calc;
     } 
+    
+    inputString1 = "";
+    inputString2 = "";
+    opert = "";
+    //inputString1 = calc;
 }
 
 
 function eraseAll() {
     inputString1 = "";
+    inputString2 = "";
     document.getElementById("display").value = inputString1;
 };
